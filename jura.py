921843fce525b9106786af1ebf38be57e392f95a
Marius van der Wijden <m.vanderwijden@live.de>
2018-02-04 17:40:29 +0100

    Added import and written the big test

--- jura.py
+++ jura.py
@@ -9,13 +9,14 @@ import struct
 import binascii
 from bitarray import bitarray
 
+
 # Given an ascii character c, returns an array (size 4) of bitarray in Jura encoding
 #
 # 4 UART bytes are one real byte:
 # MSB.....LSB
 # UART byte 33221100
 # bit 52525252 contain the information
-def tojura(c):
+def to_jura(c):
     z0 = bitarray(8)
     z0.setall(True)
     z1 = bitarray(8)
@@ -38,8 +39,9 @@ def tojura(c):
 
     return [z0,z1,z2,z3]
 
+
 # given an array of 4 bitarrays in jura encoding, return the decoded character byte
-def fromjura(bytes):
+def from_jura(bytes):
     if len(bytes) != 4: raise ValueError('Needs an array of size 4')
     c = bitarray(8)
 
@@ -54,37 +56,67 @@ def fromjura(bytes):
 
     return c.tobytes()
 
-def testencoder():
+
+def test_encoder():
     teststring = b'AN:01' # enforce ascii
     bytes = []
     for c in teststring:
-        bytes.append(tojura(c))
+        bytes.append(to_jura(c))
     chars = ""
     for arr in bytes:
-        chars += fromjura(arr)
+        chars += from_jura(arr)
     assert chars == teststring, "encoding/decoding functions must be symmetrical"
 
+
 # encodes and writes a command without CRLF to the coffee machine
-def sendcommand(cmd):
+def send_command(cmd):
     cmd += "\r\n"
     for c in cmd:
-        bytes = tojura(c)
+        bytes = to_jura(c)
         for b in bytes:
             serialport.write(b.tobytes())
             time.sleep(8 / 1000) # 8ms break between bytegroups
     return
 
+
 # receives a response of any size into a bytearray
-def receiveresponse():
+def receive_response():
     response = bytearray()
     bytes = bytearray(4)
     while serialport.in_waiting > 0:
         print 'read bytes'
         serialport.readinto(bytes)
-        response.append(fromjura(bytes))
+        response.append(from_jura(bytes))
     return response
 
-testencoder()
+
+def start_machine():
+    send_command(CMD_MACHINE_ON)
+    print receive_response()
+
+
+def stop_machine():
+    send_command(CMD_MACHINE_OFF)
+    print receive_response()
+
+
+def order_product(index):
+    if(index < len(products)):
+        print "product not available"
+    else:
+        print "ordering product " + products[index]
+        send_command(CMD_SEND_PRODUCT + products[index])
+        print receive_response()
+
+
+def the_big_test():
+    start_machine()
+    send_command(CMD_GET_MACHINE_TYPE)
+    print receive_response()
+    for index in len(products):
+        order_product(index)
+    stop_machine()
+
 
 # commands without arguments
 CMD_MACHINE_ON = b'AN:01'
@@ -95,6 +127,14 @@ CMD_READ_MACHINE_TYPE = b'RE:31'
 CMD_READ_INPUTS = b'IC:'
 CMD_GET_MACHINE_TYPE = b'TY:'
 CMD_READ_RAM = b'RR:'
+CMD_SEND_PRODUCT = b'?P' #third char is product name
+
+#if defined(S95)
+productsS95 = "EFABJIG";
+#if defined(X7)
+productsX7 = "ABCHDEKJFG";
+productsTest = "ABCDEFGHIJKLMNOPQRSTUVWKYZ"
+products = productsTest
 
 # commands that need arguments
 CMD_READ_WORD = b'RE:' # needs address
@@ -103,18 +143,18 @@ CMD_READ_LINE = b'RT:' # needs address
 print "----------------------"
 print "-  Q42 JURA HACKING  -"
 print "----------------------"
-
+test_encoder()
 try:
     serialport = serial.Serial('/dev/ttyUSB0', 9600) #9600 8 N 1
     serialport.close()
     serialport.open()
 except Exception as e:
     print e
-    print "Cannot open serial port ('" + str(e.strerror) + "'). This script requires a raspberry pi serial port connected to a Jura coffee machine."
+    print "Cannot open serial port ('" + str(e) + "'). This script requires a raspberry pi serial port connected to a Jura coffee machine."
     sys.exit()
 
-sendcommand(CMD_GET_MACHINE_TYPE)
-r = receiveresponse()
+send_command(CMD_GET_MACHINE_TYPE)
+r = receive_response()
 
 print r
 print r
